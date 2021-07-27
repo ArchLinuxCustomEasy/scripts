4df9b786dc4dfd38865fad381993f899ca43e8eb
Titux Metal <github@lgdweb.fr>
2021-07-27 08:17:52 +0200

    Updating baseInstall.sh file; umount partitions in the end of the script

--- baseInstall.sh
+++ baseInstall.sh
@@ -120,3 +120,6 @@ echo "/swapfile none swap defaults 0 0" >> /mnt/etc/fstab
 # Reduce swap usage
 echo "vm.swappiness=10
 vm.vfs_cache_pressure=50" > /mnt/etc/sysctl.d/99-swappiness.conf
+
+# Umount partitions
+umount -R /mnt
